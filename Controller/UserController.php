534d16a0bbc709774bcead99ad5dbc9959d0473f
Genar Trias <g.trias@picmedia.es>
2014-12-10 13:33:35 +0100

    Added index view

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -42,6 +42,52 @@ class UserController extends Controller
      */
     public function searchAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $rep = $em->getRepository('ACSACSPanelBundle:FosUser');
+
+        $term = $request->request->get('term');
+
+        $query = $rep->createQueryBuilder('u')
+            ->where('u.id = ?1')
+            ->orWhere('u.username LIKE ?1')
+            ->orWhere('u.email LIKE ?1')
+            ->orWhere('u.roles LIKE ?1')
+            ->orWhere('u.firstname LIKE ?1')
+            ->orWhere('u.lastname LIKE ?1')
+            ->orWhere('u.uid = ?1')
+            ->orWhere('u.gid = ?1')
+            ->setParameter('1',$term)
+            ->getQuery();
+
+        $entities = $query->execute();
+
+        return $this->render('ACSACSPanelUserBundle:User:index.html.twig', array(
+            'search_action' => 'user_search',
+            'term' => $term,
+            'entities' => $entities,
+        ));
+
     }
+
+    /**
+     * Displays a form to create a new FosUser entity.
+     *
+     * @Route("/", name="users_new")
+     */
+    public function newAction()
+    {
+        $entity = new FosUser();
+
+        $form = $this->createForm(new FosUserType(), $entity, array(
+            'em' => $this->getDoctrine()->getEntityManager(),
+        ));
+
+        return $this->render('ACSACSPanelUserBundle:FosUser:new.html.twig', array(
+            'search_action' => 'user_search',
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
 
 }
