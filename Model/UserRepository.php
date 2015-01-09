fd7545a7801af2ed771dc0d7776caed507a2c0c9
Genar Trias <g.trias@picmedia.es>
2015-01-09 18:25:10 +0100

    Moved FosGroup entitiy

--- Model/UserRepository.php
+++ Model/UserRepository.php
@@ -9,4 +9,21 @@ use Doctrine\ORM\EntityRepository;
 
 class User extends EntityRepository
 {
+    public function qbSuperadminUsers()
+    {
+        return $this->createQueryBuilder('u')
+            ->select('u')
+            ->from('ACS\ACSPanelUsersBundle\Entity\FosUser u')
+            ->innerJoin('u.group','g')
+            ->where('g.roles LIKE :roles')
+            ->setParameter('roles', '%ROLE_SUPER_ADMIN%');
+    }
+
+    public function getSuperadminUsers()
+    {
+        return $this->qbSuperadminUsers()
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
---
+++ Tests/Model/UserRepositoryTest.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace ACS\ACSPanelUsersBundle\Tests\Model;
+
+class UserRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetRegDomainWithAlias()
+    {
+        //$domain = new Domain('alias.example.com');
+        //$result = $domain->get_reg_domain();
+        //$this->assertEquals('example.com', $result);
+    }
+}
